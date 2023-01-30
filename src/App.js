41957c6b11c7820c43ee9291ef385aeb826e517f
unknown <waqassaeedbutt007@gmail.com>
2023-01-30 16:43:57 +0500

    Advance Table Done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,175 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
 
-function App() {
+import { useMemo } from 'react';
+
+//MRT Imports
+import MaterialReactTable from 'material-react-table';
+
+//Material-UI Imports
+import {
+  Box,
+  Button,
+  ListItemIcon,
+  MenuItem,
+  Typography,
+  TextField,
+} from '@mui/material';
+
+//Date Picker Imports
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { DatePicker } from '@mui/x-date-pickers/DatePicker';
+
+//Icons Imports
+import { AccountCircle, Send } from '@mui/icons-material';
+
+//Mock Data
+import  data  from './makeData.ts';
+
+function createData(
+  name: string,
+  calories: number,
+  fat: number,
+  carbs: number,
+  protein: number,
+) {
+  return { name, calories, fat, carbs, protein };
+}
+
+const rows = [
+  createData('Frozen yoghurt', 159, 6.0, 24, 4.0),
+  createData('Ice cream sandwich', 237, 9.0, 37, 4.3),
+  createData('Eclair', 262, 16.0, 24, 6.0),
+  createData('Cupcake', 305, 3.7, 67, 4.3),
+  createData('Gingerbread', 356, 16.0, 49, 3.9),
+];
+
+
+
+const App=() => {
+
+  const columns = useMemo(
+    () => [
+      {
+        id: 'employee', //id used to define `group` column
+        header: 'Employee',
+        columns: [
+          {
+            accessorFn: (row) => `${row.firstName} ${row.lastName}`, //accessorFn used to join multiple data into a single cell
+            id: 'name', //id is still required when using accessorFn instead of accessorKey
+            header: 'Name',
+            size: 250,
+            Cell: ({ renderedCellValue, row }) => (
+              <Box
+                sx={{
+                  display: 'flex',
+                  alignItems: 'center',
+                  gap: '1rem',
+                }}
+              >
+                <img
+                  alt="avatar"
+                  height={30}
+                  src={row.original.avatar}
+                  loading="lazy"
+                  style={{ borderRadius: '50%' }}
+                />
+                {/* using renderedCellValue instead of cell.getValue() preserves filter match highlighting */}
+                <span>{renderedCellValue}</span>
+              </Box>
+            ),
+          },
+          {
+            accessorKey: 'email', //accessorKey used to define `data` column. `id` gets set to accessorKey automatically
+            enableClickToCopy: true,
+            header: 'Email',
+            size: 300,
+          },
+        ],
+      },
+      {
+        id: 'id',
+        header: 'Job Info',
+        columns: [
+          {
+            accessorKey: 'salary',
+            filterVariant: 'range',
+            header: 'Salary',
+            size: 200,
+            //custom conditional format and styling
+            Cell: ({ cell }) => (
+              <Box
+                component="span"
+                sx={(theme) => ({
+                  backgroundColor:
+                    cell.getValue() < 50_000
+                      ? theme.palette.error.dark
+                      : cell.getValue() >= 50_000 && cell.getValue() < 75_000
+                      ? theme.palette.warning.dark
+                      : theme.palette.success.dark,
+                  borderRadius: '0.25rem',
+                  color: '#fff',
+                  maxWidth: '9ch',
+                  p: '0.25rem',
+                })}
+              >
+                {cell.getValue()?.toLocaleString?.('en-US', {
+                  style: 'currency',
+                  currency: 'USD',
+                  minimumFractionDigits: 0,
+                  maximumFractionDigits: 0,
+                })}
+              </Box>
+            ),
+          },
+          {
+            accessorKey: 'jobTitle', //hey a simple column for once
+            header: 'Job Title',
+            size: 350,
+          },
+          {
+            accessorFn: (row) => new Date(row.startDate), //convert to Date for sorting and filtering
+            id: 'startDate',
+            header: 'Start Date',
+            filterFn: 'lessThanOrEqualTo',
+            sortingFn: 'datetime',
+            Cell: ({ cell }) => cell.getValue()?.toLocaleDateString(), //render Date as a string
+            Header: ({ column }) => <em>{column.columnDef.header}</em>, //custom header markup
+            //Custom Date Picker Filter from @mui/x-date-pickers
+            Filter: ({ column }) => (
+              <LocalizationProvider dateAdapter={AdapterDayjs}>
+                <DatePicker
+                  onChange={(newValue) => {
+                    column.setFilterValue(newValue);
+                  }}
+                  renderInput={(params) => (
+                    <TextField
+                      {...params}
+                      helperText={'Filter Mode: Lesss Than'}
+                      sx={{ minWidth: '120px' }}
+                      variant="standard"
+                    />
+                  )}
+                  value={column.getFilterValue()}
+                />
+              </LocalizationProvider>
+            ),
+          },
+        ],
+      },
+    ],
+    [],
+  );
+
   return (
     <div className="App">
       <header className="App-header">
@@ -18,8 +186,154 @@ function App() {
           Learn React
         </a>
       </header>
+      <p>Hello</p>
+      <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell>Dessert (100g serving)</TableCell>
+            <TableCell align="right">Calories</TableCell>
+            <TableCell align="right">Fat&nbsp;(g)</TableCell>
+            <TableCell align="right">Carbs&nbsp;(g)</TableCell>
+            <TableCell align="right">Protein&nbsp;(g)</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {rows.map((row) => (
+            <TableRow
+              key={row.name}
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              <TableCell component="th" scope="row">
+                {row.name}
+              </TableCell>
+              <TableCell align="right">{row.calories}</TableCell>
+              <TableCell align="right">{row.fat}</TableCell>
+              <TableCell align="right">{row.carbs}</TableCell>
+              <TableCell align="right">{row.protein}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+
+    <MaterialReactTable
+      columns={columns}
+      data={data}
+      enableColumnFilterModes
+      enableColumnOrdering
+      enableGrouping
+      enablePinning
+      enableRowActions
+      enableRowSelection
+      initialState={{ showColumnFilters: true }}
+      positionToolbarAlertBanner="bottom"
+      renderDetailPanel={({ row }) => (
+        <Box
+          sx={{
+            display: 'flex',
+            justifyContent: 'space-around',
+            alignItems: 'center',
+          }}
+        >
+          <img
+            alt="avatar"
+            height={200}
+            src={row.original.avatar}
+            loading="lazy"
+            style={{ borderRadius: '50%' }}
+          />
+          <Box sx={{ textAlign: 'center' }}>
+            <Typography variant="h4">Signature Catch Phrase:</Typography>
+            <Typography variant="h1">
+              &quot;{row.original.signatureCatchPhrase}&quot;
+            </Typography>
+          </Box>
+        </Box>
+      )}
+      renderRowActionMenuItems={({ closeMenu }) => [
+        <MenuItem
+          key={0}
+          onClick={() => {
+            // View profile logic...
+            closeMenu();
+          }}
+          sx={{ m: 0 }}
+        >
+          <ListItemIcon>
+            <AccountCircle />
+          </ListItemIcon>
+          View Profile
+        </MenuItem>,
+        <MenuItem
+          key={1}
+          onClick={() => {
+            // Send email logic...
+            closeMenu();
+          }}
+          sx={{ m: 0 }}
+        >
+          <ListItemIcon>
+            <Send />
+          </ListItemIcon>
+          Send Email
+        </MenuItem>,
+      ]}
+      renderTopToolbarCustomActions={({ table }) => {
+        const handleDeactivate = () => {
+          table.getSelectedRowModel().flatRows.map((row) => {
+            alert('deactivating ' + row.getValue('name'));
+          });
+        };
+
+        const handleActivate = () => {
+          table.getSelectedRowModel().flatRows.map((row) => {
+            alert('activating ' + row.getValue('name'));
+          });
+        };
+
+        const handleContact = () => {
+          table.getSelectedRowModel().flatRows.map((row) => {
+            alert('contact ' + row.getValue('name'));
+          });
+        };
+
+        return (
+          <div style={{ display: 'flex', gap: '0.5rem' }}>
+            <Button
+              color="error"
+              disabled={!table.getIsSomeRowsSelected()}
+              onClick={handleDeactivate}
+              variant="contained"
+            >
+              Deactivate
+            </Button>
+            <Button
+              color="success"
+              disabled={!table.getIsSomeRowsSelected()}
+              onClick={handleActivate}
+              variant="contained"
+            >
+              Activate
+            </Button>
+            <Button
+              color="info"
+              disabled={!table.getIsSomeRowsSelected()}
+              onClick={handleContact}
+              variant="contained"
+            >
+              Contact
+            </Button>
+          </div>
+        );
+      }}
+    />
+
     </div>
-  );
+
+
+
+  )
 }
 
 export default App;
